e45a47771d7d99f7ad3f0e3b7a1547c4348a4e11
daniil efimov <danyefimoff@gmail.com>
2022-06-11 13:14:42 +0300

    db

--- server/SQLighter.py
+++ server/SQLighter.py
@@ -19,19 +19,18 @@ class SQLighter:
     def close(self):
         self.connection.close() # close the connection to the database
     
-    def get_user(self, login, password):
-        obj = self.cursor.execute(f"SELECT HOUSE_ID FROM users WHERE LOGIN = '{login}' AND PASSWORD = '{password}'")
+    def get_user(self, email, password):
+        obj = self.cursor.execute(f"SELECT full_name FROM users WHERE email = '{email}' AND PASSWORD = '{password}'")
 
         if len(obj.fetchall()) == 0:
-            return False, 0
+            return False, ''
         else:
-            obj = self.cursor.execute(f"SELECT name FROM users WHERE LOGIN = '{login}' AND PASSWORD = '{password}'")
+            obj = self.cursor.execute(f"SELECT full_name FROM users WHERE email = '{email}' AND PASSWORD = '{password}'")
             name = obj.fetchall()[0][0]
             return True, name
-
     
-    def is_user_registred(self, login):
-        obj = self.cursor.execute(f"SELECT LOGIN FROM users WHERE LOGIN = '{login}'")
+    def is_user_registred(self, email):
+        obj = self.cursor.execute(f"SELECT LOGIN FROM users WHERE email = '{email}'")
         if len(obj.fetchall()) == 0:
             return False
         else:
@@ -39,9 +38,9 @@ class SQLighter:
 
     def insert_feedback(self, user_id, content):
         self.cursor.execute(
-            f"INSERT INTO feedback (user_id,content ) VALUES( {int(user_id)},'{content}')")
+            f"INSERT INTO feedback (user_id,content) VALUES( {int(user_id)},'{content}')")
         self.connection.commit()
 
-    def register_user(self, login, password):
-        self.cursor.execute(f"INSERT INTO users (login, password) VALUES('{login}', '{password}')")
+    def register_user(self, full_name, password, inn, company_name, country, company_type, email, position):
+        self.cursor.execute(f"INSERT INTO users (full_name, password, inn, company_name, country, company_type, email, position) VALUES('{full_name}', '{password}', {inn}, '{company_name}', '{country}', '{company_type}', '{email}', '{position}')")
         self.connection.commit()
