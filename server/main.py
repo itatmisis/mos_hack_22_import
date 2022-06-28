d571045f7d0f8791e39ef34ea70305fddd62c612
Sergey Bratchikov <hivaze.me@gmail.com>
2022-06-28 23:16:59 +0300

    Removed redundant router

--- server/main.py
+++ server/main.py
@@ -1,21 +1,12 @@
 import logging
-import time
-from typing import Callable
+logging.basicConfig(format="%(asctime)s [%(name)s] - %(levelname)s: %(message)s", level=logging.INFO)
 
 from starlette.middleware.cors import CORSMiddleware
 from starlette.staticfiles import StaticFiles
-
 import models
-
-logging.basicConfig(format="%(asctime)s [%(name)s] - %(levelname)s: %(message)s", level=logging.INFO)
-
 import uvicorn
-
 from fastapi import APIRouter, FastAPI, Request, Response
-from fastapi.routing import APIRoute
-
 from database import data_source
-
 import service
 
 app = FastAPI()
@@ -32,26 +23,6 @@ app.mount("/web", StaticFiles(directory='web'), name="static")
 # app.mount("/", StaticFiles(directory="web", html=True), name="web")
 
 logger = logging.getLogger(__name__)
-
-
-class TimedRoute(APIRoute):
-    def get_route_handler(self) -> Callable:
-        original_route_handler = super().get_route_handler()
-
-        async def custom_route_handler(request: Request) -> Response:
-            before = time.time()
-            response: Response = await original_route_handler(request)
-            duration = time.time() - before
-            response.headers["X-Response-Time"] = str(duration)
-            logger.info(f"route duration: {duration}")
-            logger.info(f"route response: {response}")
-            logger.info(f"route response headers: {response.headers}")
-            return response
-
-        return custom_route_handler
-
-
-router = APIRouter(route_class=TimedRoute)
 
 
 @app.post("/user/register", response_model=models.AuthorizationResult)
@@ -98,8 +69,6 @@ def search_items(search: models.Search):
 def ping():
     return {'status': 'ok'}
 
-
-app.include_router(router)
 
 if __name__ == "__main__":
     # print(len(data_source.search_company(industry="Машиностроение")))
